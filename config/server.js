fb8d5833557baf51c3d5c3604013b68c9eebc462
Erico Alexandre <ericoabs25@gmail.com>
2021-08-26 09:10:30 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7d198c461105d1cb3b573aa20eee4bd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6622bfb9-388c-45c3-b97c-d0fe317d814b'
+};
